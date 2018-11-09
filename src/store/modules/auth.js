c5088a35ca40736f3f352c7d2d3e68364380c6cd
SparksFullStack <csparks@antioch.edu>
2018-11-09 11:04:57 -0800

    auth module all wired up

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,3 +1,5 @@
+import api from '../../api/imgur';
+
 const state = {
     token: null,
 }
@@ -7,10 +9,13 @@ const getters = {
 }
 
 const actions = {
+    login: () => {
+        // calling the login API helper method
+        api.login()
+    },
     logout: ({ commit }) => {
         commit("setToken", null);
     },
-
 }
 
 const mutations = {
@@ -19,6 +24,10 @@ const mutations = {
     }
 }
 
-// https://api.imgur.com/oauth2/authorize?client_id=YOUR_CLIENT_ID&response_type=REQUESTED_RESPONSE_TYPE&state=APPLICATION_STATE
-// ID: f99e528c1e8aad5
-// Secret: c2612178c64f1e7ca9743a707dbcaafb3fcaa691
+export default {
+    state,
+    getters,
+    actions,
+    mutations,
+}
+
